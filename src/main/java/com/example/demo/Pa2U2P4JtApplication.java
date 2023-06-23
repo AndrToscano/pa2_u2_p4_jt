58100d2673c0f6a17949ce9924616339be38a96b
juan <andro_ozp@hotmail.com>
2023-06-22 22:40:36 -0500

    Taller 16: One To One

--- src/main/java/com/example/demo/Pa2U2P4JtApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4JtApplication.java
@@ -1,22 +1,14 @@
 package com.example.demo;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.uce.edu.repository.model.CuentaBancaria;
-import com.example.demo.uce.edu.service.CuentaBancariaService;
-
 
 @SpringBootApplication
 public class Pa2U2P4JtApplication implements CommandLineRunner{
 
-	@Autowired
-	private CuentaBancariaService cuentaBancariaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P4JtApplication.class, args);
@@ -25,29 +17,7 @@ public class Pa2U2P4JtApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
-		CuentaBancaria cuentaBancaria = new CuentaBancaria();
-		cuentaBancaria.setCedulaDePropietario("1724693740");
-		cuentaBancaria.setNumero("12345");
-		cuentaBancaria.setFechaDeApertura(LocalDateTime.now());
-		cuentaBancaria.setSaldo(new BigDecimal(0));
-		cuentaBancaria.setTipo("A");
-		
-		
-		//CRUD
-		//Inicio de deposito y apertura de la cuenta
-		
-		this.cuentaBancariaService.aperturar(cuentaBancaria);
-		this.cuentaBancariaService.depositar(1, new BigDecimal(100));
 	
-		//Guardar e
-		this.cuentaBancariaService.guardar(cuentaBancaria);
-
-		//Borrar
-		this.cuentaBancariaService.borrar(1);
 		
 	}
-	
-	
-
 }
