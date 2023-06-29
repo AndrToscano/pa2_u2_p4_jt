ac57ea3173292a6f1bc8e98637dc7e7e4770e9b1
juan <andro_ozp@hotmail.com>
2023-06-29 16:39:26 -0500

    Taller 17: One To Many

--- src/main/java/com/example/demo/Pa2U2P4JtApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4JtApplication.java
@@ -33,28 +33,28 @@ public class Pa2U2P4JtApplication implements CommandLineRunner{
 		Ciudadano ciudadano = new Ciudadano();
 		Empleado empleado = new Empleado();
 		
-		ciudadano.setId(1);
+	
 		ciudadano.setApellido("Toscano");
 		ciudadano.setNombre("Juan");
 		ciudadano.setCedula("1724693740");
 		
-		empleado.setId(1);
+		
 		empleado.setCargo("Fiscalizador");
 		empleado.setSueldo(new BigDecimal(800));
 
 		//Relacion 
-		ciudadano.setEmpleado(empleado);
+		//ciudadano.setEmpleado(empleado);
 		empleado.setCiudadano(ciudadano);
 		
 		//CRUD
-		this.ciudadanoService.registrar(ciudadano);
-		this.ciudadanoService.guardar(ciudadano);
-		this.ciudadanoService.buscar(1);
-		this.ciudadanoService.borrar(1);
+		//this.ciudadanoService.registrar(ciudadano);
+		//this.ciudadanoService.guardar(ciudadano);
+		//this.ciudadanoService.buscar(1);
+		//this.ciudadanoService.borrar(1);
 		
 		this.empleadoService.registrar(empleado);
-		this.empleadoService.guardar(empleado);
-		this.empleadoService.buscar(1);
-		this.empleadoService.borrar(1);
+		//this.empleadoService.guardar(empleado);
+		//this.empleadoService.buscar(1);
+		//this.empleadoService.borrar(1);
 	}
 }
