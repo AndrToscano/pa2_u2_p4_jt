98a362299db30d5f697f28b7e6c07f9ab2ab5a24
juan <andro_ozp@hotmail.com>
2023-06-30 14:02:25 -0500

    Taller 19: Many To many - Tabla Rompimiento

--- src/main/java/com/example/demo/Pa2U2P4JtApplication.java
+++ src/main/java/com/example/demo/Pa2U2P4JtApplication.java
@@ -1,23 +1,25 @@
 package com.example.demo;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.uce.edu.repository.model.Autor;
-import com.example.demo.uce.edu.repository.model.Libro;
-import com.example.demo.uce.edu.service.LibroService;
+import com.example.demo.uce.edu.repository.model.Alumno;
+import com.example.demo.uce.edu.repository.model.Materia;
+import com.example.demo.uce.edu.repository.model.Matricula;
+import com.example.demo.uce.edu.service.MatriculaService;
 
 
 @SpringBootApplication
 public class Pa2U2P4JtApplication implements CommandLineRunner{
 
 	@Autowired
-    private LibroService libroService;
+    private MatriculaService matriculaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U2P4JtApplication.class, args);
@@ -26,72 +28,32 @@ public class Pa2U2P4JtApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 	
-		Autor autor1 = new Autor();
-		Autor autor2 = new Autor();
-		Autor autor3 = new Autor();
+		Alumno alumno = new Alumno();
+		Materia materia = new Materia();
 		
-		Libro libro1 = new Libro();
-		Libro libro2 = new Libro();
+		Matricula matricula = new Matricula();
+		List<Matricula> matriculas = new ArrayList<>();
 		
+		alumno.setNombre("David");
 		
-		Set<Autor> autores1 = new HashSet<>();
-		Set<Autor> autores2 = new HashSet<>();
+		materia.setNombre("Inteligencia Artificial");
 		
-		Set<Libro> libros = new HashSet<>();
+		matricula.setNumeroDeMatricula("C005");
+		matricula.setFechaDeMatricula(LocalDateTime.now());
 		
+		matriculas.add(matricula);
+	
 		
-		
-		//Autor
-		autor1.setApellido("Cortazar");
-		autor1.setNombre("Andres");
-		
-		autor2.setApellido("Silva");
-		autor2.setNombre("Carlos");
-		
-		autor3.setApellido("Freire");
-		autor3.setNombre("Daniela");
-		
-		//Libro
-		libro1.setTitulo("Lo que el viento se llevo");
-		libro1.setEditorial("AYLLA");
-		
-		
-		libro2.setTitulo("See you");
-		libro2.setEditorial("London");
-		
-		
-		libros.add(libro1);
-		libros.add(libro2);
-		
-		
-		
-		autores1.add(autor1);
-		autores1.add(autor2);
-		
-		autores2.add(autor3);
-		
-		//Relacion
-		autor1.setLibros(libros);
-		autor2.setLibros(libros);
-		autor3.setLibros(libros);
-		
-		libro1.setAutores(autores1);
-		libro2.setAutores(autores2);
+		alumno.setMatriculas(matriculas);
+		materia.setMatriculas(matriculas);
+		matricula.setAlumno(alumno);
+		matricula.setMateria(materia);
 		
 		
 		//CRUD
-		
-		this.libroService.registrar(libro1);
-		this.libroService.registrar(libro2);
-		
-		this.libroService.guardar(libro1);
-		this.libroService.guardar(libro2);
-		
-		this.libroService.buscar(1);
-		this.libroService.buscar(2);
-		
-		this.libroService.borrar(1);
-		this.libroService.borrar(2);
-	
+		this.matriculaService.registrar(matricula);
+		this.matriculaService.guardar(matricula);
+		this.matriculaService.buscar(1);
+		this.matriculaService.borrar(1);
 	}
 }
